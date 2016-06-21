a5149e03a1a98eab497f0b37e6ee852e70ddbaa2
solairerove <hax2033@gmail.com>
2016-06-21 12:06:19 +0300

    added xml portlet configuration
    - back in USSR

--- src/main/webapp/systemjs.config.js
+++ src/main/webapp/systemjs.config.js
@@ -1,10 +1,10 @@
 /**
  * Created by union on 20.06.16.
  */
-// System.config({
-//     baseURL: '/app/',
-//
-//     path: {
-//         'angular2': 'node_modules/angular2/bundles/angular2.dev.js'
-//     }
-// });
+System.config({
+    baseURL: '/app/',
+
+    path: {
+        'angular2': 'node_modules/angular2/bundles/angular2.dev.js'
+    }
+});
