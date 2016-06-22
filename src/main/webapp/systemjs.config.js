7cb270b1d8e0b44bc1bf205746bfb21aa65b1d21
solairerove <hax2033@gmail.com>
2016-06-22 09:20:00 +0300

    prepare to rebuild system.config.js

--- src/main/webapp/systemjs.config.js
+++ src/main/webapp/systemjs.config.js
@@ -5,6 +5,7 @@ System.config({
     baseURL: '/app/',
 
     path: {
-        'angular2': 'node_modules/angular2/bundles/angular2.dev.js'
+        'angular2': 'node_modules/angular2/bundles/angular2.dev.js',
+        'angular2-polyfills': 'node_modules/angular2/bundles/angular2-polyfills.js'
     }
 });
