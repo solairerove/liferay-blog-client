2333e36db24a65c5700843efcd3c84889441e4cb
solairerove <hax2033@gmail.com>
2016-06-22 14:03:35 +0300

    system.js configure

--- src/main/webapp/systemjs.config.js
+++ src/main/webapp/systemjs.config.js
@@ -1,17 +1,11 @@
-/**
- * System configuration for Angular 2 samples
- * Adjust as necessary for your application needs.
- */
-
 (function(global) {
-    // map tells the System loader where to look for things
+
     var map = {
         'app':                        'app', // 'dist',
         'angular2':                   'node_modules/angular2',
-        'rxjs':                       'node_modules/rxjs',
-        'tether': 'https://cdnjs.cloudflare.com/ajax/libs/tether/1.2.0/js/tether.js'
+        'rxjs':                       'node_modules/rxjs'
     };
-    // packages tells the System loader how to load when no filename and/or no extension
+
     var packages = {
         'app':                        { main: 'main.js',  defaultExtension: 'js' },
         'rxjs':                       { defaultExtension: 'js' }
@@ -21,9 +15,10 @@
         'angular2/core'
     ];
     
-    // filterSystemConfig - index.html's chance to modify config before we register it.
     if (global.filterSystemConfig) { global.filterSystemConfig(config); }
     
     System.config(config);
+
+    System.import('app/main').then(null, console.error.bind(console));
 
 })(this);
