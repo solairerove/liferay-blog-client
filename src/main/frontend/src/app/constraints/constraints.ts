9542d1692bb6ad9f6e2129dd5eeca233cdcf6f80
solairerove <hax2033@gmail.com>
2016-06-27 15:59:37 +0300

    change api url in constaints

--- src/main/frontend/src/app/constraints/constraints.ts
+++ src/main/frontend/src/app/constraints/constraints.ts
@@ -5,6 +5,6 @@ export class Constraints {
     public static ADD_POST_PATH = '/add';
     public static LOG_IN_PATH = '/authenticate';
     public static OTHER = '/**';
-    public static POST_URL = 'http://localhost:8080/api/posts';
+    public static POST_URL = 'http://localhost:8080/liferay-blog-backend-0.0.1-SNAPSHOT/api/posts';
     public static API_URL = 'http://localhost:8080/api';
 }
